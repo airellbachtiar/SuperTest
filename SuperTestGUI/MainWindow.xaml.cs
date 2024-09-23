0213d1bc61f8e066881379acafe2cbbd3137b68c
Airell Bachtiar <airell.bachtiar@sioux.eu>
2024-09-23 09:07:21 +0200

    Added WPF UI

---
+++ SuperTestGUI/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using Microsoft.Win32;
+using ReqIFSharp;
+
+namespace SuperTestGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ReqIF reqIF = new ReqIF();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void UploadReqIFButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "ReqIF (*.reqif)|*.reqif|Text Files (*.txt)|*.txt|All Files (*.*)|*.*";
+
+            bool? response = openFileDialog.ShowDialog();
+
+            if (response != true) return;
+
+            string filepath = openFileDialog.FileName;
+            labelReqIFFile.Content = filepath;
+
+            ReqIFDeserializer deserializer = new ReqIFDeserializer();
+            reqIF = deserializer.Deserialize(filepath).First();
+
+            XmlDocument doc = new XmlDocument();
+
+            foreach (SpecObject specObject in reqIF.CoreContent.SpecObjects)
+            {
+                string listString = String.Join(", ", specObject.Values.Select(val => val.ObjectValue.ToString().RemoveXhtmlTags()));
+
+                requirementsTreeViewer.Items.Add(listString);
+            }
+        }
+    }
+}
---
+++ SuperTestGUI/SpecElementWithAttributesExtensions.cs
@@ -0,0 +1,44 @@
+﻿using ReqIFSharp;
+using HtmlAgilityPack;
+
+namespace SuperTestGUI
+{
+    public static class SpecElementWithAttributesExtensions
+    {
+        public static object ExtractDisplayName(this SpecElementWithAttributes specElementWithAttributes)
+        {
+            if (string.IsNullOrEmpty(specElementWithAttributes.LongName))
+            {
+                var attributeValue = specElementWithAttributes.Values.FirstOrDefault();
+                if (attributeValue != null)
+                {
+                    if (attributeValue is AttributeValueXHTML attributeValueXhtml)
+                    {
+                        return RemoveXhtmlTags(attributeValueXhtml.TheValue);
+                    }
+                    else
+                    {
+                        return attributeValue.ObjectValue?.ToString() ?? string.Empty;
+                    }
+                }
+                else
+                {
+                    return specElementWithAttributes.Identifier;
+                }
+            }
+            else
+            {
+                return specElementWithAttributes.LongName;
+            }
+        }
+
+        public static string RemoveXhtmlTags(this string? xhtml)
+        {
+            if (string.IsNullOrEmpty(xhtml)) return string.Empty;
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(xhtml);
+            return doc.DocumentNode.InnerText;
+        }
+    }
+}
