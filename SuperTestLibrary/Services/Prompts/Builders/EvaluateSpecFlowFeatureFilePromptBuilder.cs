fca32027a1c243964cb629b214c296210c3fd7ef
Airell Bachtiar <airell.bachtiar@sioux.eu>
2024-10-17 11:46:27 +0200

    Added EvaluateSpecFlowFeatureFile PromptBuilder and Generator

---
+++ SuperTestLibrary/Services/Prompts/Builders/EvaluateSpecFlowFeatureFilePromptBuilder.cs
@@ -0,0 +1,70 @@
+﻿
+using System.Text;
+
+namespace SuperTestLibrary.Services.Prompts.Builders
+{
+    public class EvaluateSpecFlowFeatureFilePromptBuilder : IPromptBuilder
+    {
+        private Prompt? prompt;
+        private string? requirements;
+        private string featureFile;
+
+        public EvaluateSpecFlowFeatureFilePromptBuilder(string requirements, string featureFile)
+        {
+            if (string.IsNullOrEmpty(featureFile))
+            {
+                throw new ArgumentNullException(nameof(featureFile));
+            }
+
+            this.featureFile = featureFile;
+            this.requirements = requirements;
+        }
+
+        public IEnumerable<string> BuildPrompt(Prompt prompt)
+        {
+            this.prompt = prompt;
+
+            ArgumentNullException.ThrowIfNull(prompt);
+
+            if (string.IsNullOrEmpty(requirements))
+            {
+                throw new ArgumentNullException(nameof(requirements));
+            }
+
+            List<string> prompts = [BuildContext()];
+            return prompts;
+        }
+
+        private string BuildContext()
+        {
+            StringBuilder promptBuilder = new();
+
+            promptBuilder.AppendLine(prompt!.SystemInstruction);
+            promptBuilder.AppendLine();
+
+            foreach(var score in prompt.ScoringScale)
+            {
+                promptBuilder.AppendLine(score);
+            }
+
+            foreach (var instruction in prompt.Criteria.Select((value, i) => new { i, value }))
+            {
+                promptBuilder.AppendLine($"{instruction.i + 1}. {instruction.value}");
+            }
+
+            foreach (var instruction in prompt.Instructions)
+            {
+                promptBuilder.AppendLine(instruction);
+            }
+
+            promptBuilder.AppendLine();
+            promptBuilder.AppendLine("Feature file:");
+            promptBuilder.AppendLine(featureFile);
+            promptBuilder.AppendLine();
+            promptBuilder.AppendLine("Requirements:");
+            promptBuilder.AppendLine(requirements);
+
+            return promptBuilder.ToString();
+        }
+    }
+}
--- SuperTestLibrary/Services/Prompts/Prompt.cs
+++ SuperTestLibrary/Services/Prompts/Prompt.cs
@@ -7,5 +7,9 @@
         public string Thinking { get; init; } = string.Empty;
         public string Example { get; init; } = string.Empty;
         public IEnumerable<Interaction> Interactions { get; init; } = [];
+
+        //Evaluation
+        public IEnumerable<string> ScoringScale { get; init; } = [];
+        public IEnumerable<string> Criteria { get; init; } = [];
     }
 }
