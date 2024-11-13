bb5e5dda5be7bbbe300a1f1df22b41f39f313c03
Airell Bachtiar <airell.bachtiar@sioux.eu>
2024-11-13 14:42:57 +0100

    Added step definitions

---
+++ Traffic/TrafficTest/StepDefinitions/TrafficTestStepDefinition.cs
@@ -0,0 +1,37 @@
+﻿namespace TrafficTest.StepDefinitions
+{
+    [Binding]
+    public class TrafficTestStepDefinition
+    {
+
+        #region Traffic light system starts in red light
+        [Given("the traffic light system starts")]
+        public async void GivenTheTrafficLightSystemStarts()
+        {
+        }
+
+        [When("the system initializes")]
+        public void WhenTheSystemInitializes()
+        {
+        }
+
+        [Then("the traffic light should be red")]
+        public async void ThenTheTrafficLightShouldBeRed()
+        {
+        }
+        #endregion
+
+        #region Pedestrian light system starts in red light
+        [Given("the pedestrian light system starts")]
+        public async void GivenThePedestrianLightSystemStarts()
+        {
+        }
+
+        [Then("the pedestrian light should be red")]
+        public void ThenThePedestrianLightShouldBeRed()
+        {
+
+        }
+        #endregion
+    }
+}
